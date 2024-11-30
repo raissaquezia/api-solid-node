3374f5afee1b7f06404b36d33ceaed2fa8867ec6
Raíssa Passos <raissaquezia123@gmail.com>
2024-11-30 19:28:25 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify'
+import {PrismaClient} from "@prisma/client";
+
+export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: "John Doe",
+        email: "john@example.com",
+    }
+})
+
