949e11a51cd47da948e1122dd8dd0be7ca037cc9
Raíssa Passos <raissaquezia123@gmail.com>
2024-12-04 20:02:09 -0300

    feat: created register service

---
+++ src/services/register.ts
@@ -0,0 +1,33 @@
+import { hash } from "bcryptjs";
+import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UsersRepository } from "@/repositories/users-repository";
+import { EmailAlreadyExistsError } from "@/services/errors/email-already-exists-error";
+
+interface RegisterServiceRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class RegisterUserService {
+  constructor(private userRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: RegisterServiceRequest) {
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await this.userRepository.findUserByEmail(email);
+
+    if (userWithSameEmail) {
+      throw new EmailAlreadyExistsError();
+    }
+
+    const user = await this.userRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+
+    return user;
+  }
+}
