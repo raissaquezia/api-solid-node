2bfdde935cacf4e635d7217c2d3913ff13839278
Raíssa Passos <raissaquezia123@gmail.com>
2024-12-04 20:03:31 -0300

    feat: created service for error handling

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
-import fastify from 'fastify'
-import {PrismaClient} from "@prisma/client";
+import fastify from "fastify";
+import { appRoutes } from "@/http/routes";
+import { ZodError } from "zod";
+import { env } from "@/env";
 
-export const app = fastify()
+export const app = fastify();
 
-const prisma = new PrismaClient()
+app.register(appRoutes);
 
-prisma.user.create({
-    data: {
-        name: "John Doe",
-        email: "john@example.com",
-    }
-})
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation Error", issues: error.format() });
+  }
 
+  if (env.NODE_ENV != "production") {
+    console.error(error);
+  }
+
+  return reply.status(500).send({ message: "Internal Server Error" });
+});
